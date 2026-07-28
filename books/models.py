804ed26925a02b2944b1f6939e21ab12f4012573
Diana <ddmitrieva098@example.com>
2026-07-28 16:25:20 +1100

    добавлена модель для books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from isort.place import module_with_reason
 
-# Create your models here.
+from author.models import Author
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Название книги",
+        help_text="Укажите название книги",
+    )
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="authors",
+        verbose_name="Автор",
+        help_text="Укажите автора книги",
+    )
+    genre = models.CharField(max_length=150, verbose_name="Жанр книги", help_text="Укажите жанр", **NULLABLE)
+    description = models.TextField(verbose_name="Описание книги", **NULLABLE)
+    quantity = models.PositiveSmallIntegerField(verbose_name="Количество книг")
+    publication_date = models.DateField(verbose_name="Дата публикации", **NULLABLE)
+    publisher = models.CharField(max_length=150, verbose_name="Издатель")
