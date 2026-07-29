827c1b291a1774e687d1cd9a11aae006394dcc0f
Diana <ddmitrieva098@example.com>
2026-07-29 15:33:10 +1100

    регистрация моделей в админке

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book, Genre, Category
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "genre",
+        "description",
+        "quantity",
+        "publication_date",
+        "publisher",
+        "cover_image",
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name", "category",)
