f1fc678f73d134a410b6f91940325a460a3b08d7
Diana <ddmitrieva098@example.com>
2026-07-29 14:45:55 +1100

    добавлены модель "Author"

--- author/admin.py
+++ author/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    exclude = ("photo",)
