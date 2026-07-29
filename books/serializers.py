d4f62731dbc4fd693a5417e4cb7ba40948940975
Diana <ddmitrieva098@example.com>
2026-07-29 14:27:27 +1100

    добавлена сериалайзеры для моделей "Book, Genre, Category"

---
+++ books/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from books.models import Book, Category, Genre
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
