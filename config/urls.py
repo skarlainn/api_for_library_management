2e19157f53d4e47559b0ed135e886b34dd99172c
Diana <ddmitrieva098@example.com>
2026-07-29 15:14:59 +1100

    добавлен сериалайзер для author

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("books/", include("books.urls", namespace="books")),
     path("users/", include("users.urls", namespace="users")),
-    #path("authors/", include("author.urls", namespace="authors")),
+    path("authors/", include("author.urls", namespace="authors")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
