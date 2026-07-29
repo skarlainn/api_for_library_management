4268cf254ea02d4035f5e0609f56215f5364080d
Diana <ddmitrieva098@example.com>
2026-07-29 14:57:40 +1100

    добавлено BookViewSet, GenreViewSet, CategoryViewSet

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Book, Genre, Category
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = BookSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = BookSerializer
