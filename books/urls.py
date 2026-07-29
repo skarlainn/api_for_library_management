9ac5bfc97f0027460ed31d59c33c971f7c3d57c9
Diana <ddmitrieva098@example.com>
2026-07-29 14:44:24 +1100

    добавлены urls

---
+++ books/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from books.views import BookViewSet, GenreViewSet, CategoryViewSet
+from rest_framework.routers import DefaultRouter
+
+from config.urls import urlpatterns
+
+router = DefaultRouter()
+router.register(r"", BookViewSet, basename="books")
+router.register(f"genres", GenreViewSet, basename="genres")
+router.register(r"categories", CategoryViewSet, basename="categories")
+
+from books.apps import BooksConfig
+
+app_name = BooksConfig.name
+
+urlpatterns = [path("", include(router.urls))]
--- config/settings.py
+++ config/settings.py
@@ -29,8 +29,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "users",
     "books",
+    "author",
 ]
 
 MIDDLEWARE = [
@@ -113,3 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
