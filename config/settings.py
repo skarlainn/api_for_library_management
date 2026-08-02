b7d152e9015ae6cceb1e70e918bf550496e1b69e
Diana <ddmitrieva098@example.com>
2026-08-02 18:20:00 +1100

    добавлены маршруты

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
+    "rest_framework_simplejwt",
     "rest_framework",
     "users",
     "books",
@@ -125,3 +127,17 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path("register/", UserCreateAPIView.as_view(), name="register-user"),
+    path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user-detail"),
+    path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user-update"),
+    path("<int:pk>/delete/", UserDestroyAPIView.as_view(), name="delete"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
