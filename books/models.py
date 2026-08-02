7daee4ceb1a4bade84c49c81fa2ba69cb1cadb08
Diana <ddmitrieva098@example.com>
2026-08-02 17:25:12 +1100

    добавлена модель RentBooks и удалена модель Category

--- books/models.py
+++ books/models.py
@@ -1,29 +1,15 @@
 from django.db import models
-
 from author.models import Author
+from users.models import User
 
 NULLABLE = {"blank": True, "null": True}
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name="Название категории")
-    description = models.TextField(verbose_name="Описание категории", **NULLABLE)
-
-    def __str__(self):
-        return f"{self.name}"
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
-        ordering = ["name"]
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=150, verbose_name="Название жанра")
     description = models.TextField(verbose_name="Описание жанра", **NULLABLE)
-    category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, verbose_name="Категория", **NULLABLE
-    )
+
 
     def __str__(self):
         return f"{self.name}"
@@ -59,6 +45,7 @@ class Book(models.Model):
     publication_date = models.DateField(verbose_name="Дата публикации", **NULLABLE)
     publisher = models.CharField(max_length=150, verbose_name="Издатель")
     cover_image = models.ImageField(upload_to="books/cover_image", verbose_name="Обложка", **NULLABLE)
+    is_available = models.BooleanField(default=True, help_text="Доступна ли книга для выдачи")
 
     def __str__(self):
         return f"{self.title} - {self.author}"
@@ -67,3 +54,28 @@ class Book(models.Model):
         verbose_name = "книга"
         verbose_name_plural = "книги"
         ordering = ["title"]
+
+
+class RentBooks(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        verbose_name="Книга",
+        related_name="books",
+        help_text="Укажите книгу которую пользователь берет для чтения",
+    )
+    date_issue = models.DateField(auto_now_add=True, verbose_name="Дата выдачи книги")
+    return_date = models.DateField(verbose_name="Дата возврата книги")
+    is_returned = models.BooleanField(default=False, verbose_name="Флаг возврата книги")
+    user = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING, verbose_name="Читатель книги", help_text="Читатель книги"
+    )
+    term = models.PositiveSmallIntegerField(verbose_name="Срок аренды", help_text="Срок аренды в днях")
+
+    def __str__(self):
+        return f"Книга: {self.book} у {self.user} на {self.term} дней"
+
+    class Meta:
+        verbose_name = "Аренда книги"
+        verbose_name_plural = "Аренды книг"
+        ordering = ["pk"]
