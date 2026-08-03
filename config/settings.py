69419c17a0af4ef2d8d985af734e356d8c8d71d3
Diana <ddmitrieva098@example.com>
2026-08-03 20:44:17 +1100

    settings email, celery

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_filters",
     "rest_framework_simplejwt",
+    "django_celery_beat",
+    "drf_yasg",
     "rest_framework",
     "users",
     "books",
@@ -140,4 +142,27 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_SSL = False if os.getenv("EMAIL_USE_SSL") else True
+EMAIL_USE_TLS = True if os.getenv("EMAIL_USE_TLS") else False
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULE = {
+    "send_information": {
+        "task": "books.tasks.check_return_book",
+        "schedule": timedelta(days=1),
+    },
 }
--- users/serializers.py
+++ users/serializers.py
@@ -8,12 +8,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = (
-            "password",
             "is_superuser",
             "is_staff",
             "groups",
             "user_permissions",
         )
         validators = [
-            PhoneNumberValidator(field1="phone"),
+            PhoneNumberValidator(field_name="phone"),
         ]
+
+
+class UserSerializerForRentBooks(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "email",
+            "phone",
+        )
